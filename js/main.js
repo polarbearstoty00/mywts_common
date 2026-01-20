0aaae55e6d9633608a90cefc55e9f3c4003bb687
polarbearstoty00 <polarbearstoty00@gmail.com>
2026-01-20 16:27:22 +0900

    Update and rename main.js to categories.json

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const startBtn = document.getElementById("startBtn");
-const titleScreen = document.getElementById("title-screen");
-const mainMenu = document.getElementById("main-menu");
-
-startBtn.addEventListener("click", () => {
-    titleScreen.style.display = "none";
-    mainMenu.style.display = "block";
-});
